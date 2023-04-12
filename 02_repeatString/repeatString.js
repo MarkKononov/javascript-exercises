098cddce7cdab4f179d27525882ef780da9c6bc3
MarkKononov <markkononov01@gmail.com>
2023-04-12 13:50:30 -0700

    Solved 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, numRepeats) {
+    if(numRepeats < 0) {
+        return 'ERROR';
+    }
 
+    let finalString = '';
+
+    for(let i = 0; i < numRepeats; i++) {
+        finalString += stringToRepeat;
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
