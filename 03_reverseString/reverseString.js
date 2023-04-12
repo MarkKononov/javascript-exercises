931f6ae17aa522baf31fa618a4ff5605b286126b
MarkKononov <markkononov01@gmail.com>
2023-04-12 14:19:13 -0700

    Solved 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let stringLen = stringToReverse.length;
+    if(stringLen == 0) {
+        return '';
+    }
+    else if(stringLen == 1) {
+        return stringToReverse;
+    }
 
+   let reversedString = stringToReverse.slice(-1);
+
+   for(let i = stringLen - 2; i >= 0; i--) {
+        reversedString = reversedString + stringToReverse.slice(i, i + 1);
+   }
+
+   return reversedString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
