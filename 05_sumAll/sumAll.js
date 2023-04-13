f0437218d03234118a10acb2c2299961dd7aa945
MarkKononov <markkononov01@gmail.com>
2023-04-12 23:08:06 -0700

    Finished 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if(numOne < 0 || numTwo < 0 || typeof(numOne) !== 'number' || typeof(numTwo) !== 'number') {
+        return 'ERROR';
+    }
 
+    let smallNum = numOne;
+    let bigNum = numTwo;
+
+    if(bigNum < smallNum) {
+        smallNum = numTwo;
+        bigNum = numOne;
+    }
+
+    let totalSum = smallNum;
+    
+    for(let i = smallNum + 1; i <= bigNum; i++) {
+        totalSum += i;
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
